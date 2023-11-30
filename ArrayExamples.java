ad7af49983efed1b15c33fce5e9849cf81614209
skandhwa <113922787+skandhwa@users.noreply.github.com>
2023-11-30 18:32:07 +0530

    Add files via upload

---
+++ ArrayExamples.java
@@ -0,0 +1,43 @@
+package TestJanbask;
+
+public class ArrayExamples {
+
+	public static void main(String[] args) {
+		
+		int []a= {5,7,12,34,56};
+		
+		
+		int m=a.length;
+		System.out.println("The length of the array is "+m);
+		
+		
+		for(int x:a)
+		{
+			System.out.print(x+" ");
+		}
+		
+		System.out.println();
+		
+		System.out.println(a[2]);
+		
+		
+		
+		String []b= {"UFT","Selenium","Parasoft","LoadRunner"};
+		
+		char []c= {'A','B','C','D'};
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
